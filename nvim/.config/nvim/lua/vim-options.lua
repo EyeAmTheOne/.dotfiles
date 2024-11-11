6af7fb1e4f5a1d0a6ba0ef47c96b623868dc3f5f
Gavin <gaqw0210@gmail.com>
2024-11-11 09:03:40 -0800

    Add nvim config files

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.cmd("inoremap kj <Esc>")
+vim.cmd("inoremap jk <Esc>")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.g.mapleader = ' '
+
+
